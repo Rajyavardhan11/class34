a18edcf23a4697f43362ac60bdb9c29abdd70a07
Rajyavardhan11 <79706040+Rajyavardhan11@users.noreply.github.com>
2021-05-02 13:21:35 +0530

    Add files via upload

---
+++ Rock.js
@@ -0,0 +1,20 @@
+class Rock{
+    constructor(x,y,width,height){
+      var options ={ isStatic: false}
+      
+      this.body =  Bodies.rectangle(x,y,width,height,options);
+      World.add(myWorld,this.body);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("images/rock1.png");
+    }
+    show()
+    {   var pos = this.body.position;
+        imageMode(CENTER);
+        fill ('black');
+        stroke ("grey");
+        image(this.image, pos.x, pos.y, this.width, this.height);
+    }
+  
+  
+  }
